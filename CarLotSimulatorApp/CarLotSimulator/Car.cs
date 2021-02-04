13f07309bf014c72438262cd1326d0bee6fd56be
Norman Calhoun <shakauluz73@yahoo.com>
2021-02-04 17:24:20 -0500

    finished

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        
+        public int Year { get; set; }
+
+        public string EngineNoise { get; set; }
+
+        public string HonkNoise { get; set; }
+
+        public bool IsDriveable { get; set;  }
+
+        public Car(string make, string model, int year, string engineNoise, string honkNoise, bool isDriveable) 
+        {
+            Make =              make;
+            Model =             model;
+            Year =              year;
+            EngineNoise =       engineNoise;
+            HonkNoise =         honkNoise;
+            IsDriveable =       isDriveable;
+
+        }
+
+        public void MakeEngineNoise(string engineNoise)
+        {
+            Console.WriteLine($"The engine {engineNoise}!\n");
+        }
+
+        public void MakeHonkNoise(string honkNoise)
+        {
+            Console.WriteLine($"The horn makes this sound {honkNoise}!\n");
+        }              
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 
 namespace CarLotSimulator
 {
@@ -28,6 +29,84 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+
+            //Creat and object
+            var carsOnLot = new CarLot();
+
+            var carLotsCar1 = new Car()
+            {
+                Make =          "Ford",
+                Model =         "Mustang GT",
+                Year =          2021,
+                EngineNoise =   "Loud",
+                HonkNoise =     "Dukes of Hazard",
+                IsDriveable =   true,                
+            };
+
+            carsOnLot.Cars.Add(carLotsCar1);
+
+
+                
+
+            Car carLotsCar2 = new Car();
+
+            carLotsCar2.Make =              "Honda";
+            carLotsCar2.Model =             "Accord";
+            carLotsCar2.Year =               2021;
+            carLotsCar2.EngineNoise =       "Loud";
+            carLotsCar2.HonkNoise =         "Beep";
+            carLotsCar2.IsDriveable =        true;
+
+            carsOnLot.Cars.Add(carLotsCar2);
+
+
+            
+
+            Car carLotsCar3 = new Car("Dodge", "Charger SRT", 2021, "Lions Roar", "Get Over Here!", true);
+
+             carsOnLot.Cars.Add(carLotsCar3);
+
+
+            Car carLotsCar4 = new Car();
+
+            carLotsCar4.Make = "Mercedes";
+            carLotsCar4.Model = "Benz";
+            carLotsCar4.Year = 2021;
+            carLotsCar4.EngineNoise = "Quiet";
+            carLotsCar4.HonkNoise = "Mozart";
+            carLotsCar4.IsDriveable = true;
+
+            carsOnLot.Cars.Add(carLotsCar4);
+
+
+
+            carLotsCar1.MakeEngineNoise(carLotsCar1.EngineNoise);
+
+            carLotsCar2.MakeEngineNoise(carLotsCar2.EngineNoise);
+
+            carLotsCar3.MakeEngineNoise(carLotsCar3.EngineNoise);
+
+            carLotsCar4.MakeEngineNoise(carLotsCar4.EngineNoise);
+
+
+
+
+
+            carLotsCar1.MakeHonkNoise(carLotsCar1.HonkNoise);
+
+            carLotsCar2.MakeHonkNoise(carLotsCar2.HonkNoise);
+
+            carLotsCar3.MakeHonkNoise(carLotsCar3.HonkNoise);
+
+            carLotsCar4.MakeHonkNoise(carLotsCar4.HonkNoise);
+
+
+
+            foreach (var vehicle in carsOnLot.Cars) 
+            {
+                Console.WriteLine($"Car Year: {vehicle.Year}            Make: {vehicle.Make}               Model. {vehicle.Model }\n");
+            } 
+
         }
     }
 }
